00fc87a8299d51dac52f2350669026208735e0b9
Michael Andrea <mhandrea@uw.edu>
2014-10-26 23:11:48 -0700

    finished the states dropdown list

--- js/signup.js
+++ js/signup.js
@@ -3,22 +3,13 @@
     This script will load the state select list and validate the form before submission
 */
 
-document.addEventListener("DOMContentLoaded", get_states);
-
-function get_states() {
-	var code;
-	var name;
-	var states;
+document.addEventListener("DOMContentLoaded", function() {
 	var i;
-	var form = document.getElementByClassName('')
-	for (i = 0; i < usStates.length; ++i) {
-		code = usStates[i]["code"];
-		name = usStates[i]["name"];
-		var x = document.createElement("OPTION");
-		x.setAttribute(code, 'state');
-		var t = document.createTextNode(name);
-		x.appendChild(t);
-		document.getElement().appendChild(x);
+	for (i = 0; i < usStates.length; ++i) {	
+		var elem = document.createElement("OPTION");
+		var state = usStates[i];
+		elem.value = state["code"];
+		elem.text = state["name"];
+		document.getElementsByName('state')[0].appendChild(elem);
 	}
-}
-
+});
